be248c269bad34042a0b0d07b0b0d071b16bbebb
Aliakbar <izad.aliakbar@gmail.com>
2024-07-02 17:46:33 -0400

    Updating the data_load_AK without the collate function

--- gns/data_loader_AK.py
+++ gns/data_loader_AK.py
@@ -1,18 +1,20 @@
+from gns import seed_util
+
+seed_util.apply_seed()
+
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 import numpy as np
+from intervaltree import IntervalTree
 
 
-def load_npz_data(file_path):
+def load_npz_file(file_path):
     """
     Load data stored in npz format.
-
     The file format for Python 3.9 or less supports ragged arrays and Python 3.10
     requires a structured array. This function supports both formats.
-
     Args:
         file_path (str): Path to npz file.
-
     Returns:
         data (list): List of tuples of the form (positions, particle_type).
     """
@@ -23,23 +25,168 @@ def load_npz_data(file_path):
         raise FileNotFoundError(f"File '{file_path}' not found.")
 
 
+class TrajectoriesSampleDataset(Dataset):
+    """
+    Trajectories dataset where each trajectory is a series of tuples
+        (positions, particle_type, material_property[optional]).
+    positions' shape: (sequence_length, n_particle, dimension)
+        where dimension is the number of coordinates (e.g. 2 for x and y)
+    particle type shape: (n_particles)
+    [optional] material_property: (n_particles, feature vector)
+
+    This dataset returns a sequence of a trajectory with "input_length_sequence" length.
+    """
+
+    def __init__(self, root_dir, input_length_sequence, transform=None):
+        super().__init__()
+        self._input_length_sequence = input_length_sequence
+        self._trajectories_data = load_npz_file(root_dir)
+        # computing the length of each trajectory (# sequence) subtracting the input length sequence
+        self._trajectories_length = [
+            x.shape[0] - input_length_sequence for x, *_ in self._trajectories_data
+        ]
+        self._dataset_length = sum(self._trajectories_length)
+
+        # Initialize the interval tree
+        self._interval_tree = IntervalTree()
+        current_end = 0
+        for i, length in enumerate(self._trajectories_length):
+            self._interval_tree[current_end : current_end + length] = i
+            current_end += length
+
+    def __len__(self):
+        return self._dataset_length
+
+    def __getitem__(self, idx):
+        """
+        Return a sequence of points in a trajectory at index idx with length _input_length_sequence
+        """
+        # Finding tuple with index idx belongs to which trajectory
+        interval = list(self._interval_tree[idx])[0]
+        trajectory_idx = interval.data
+        positions, particle_type, *_material_property = self._trajectories_data[
+            trajectory_idx
+        ]
+
+        trajectory_first_index = interval.begin
+        tuple_idx_in_trajectory = idx - trajectory_first_index
+
+        positions_in_sample = positions[
+            tuple_idx_in_trajectory : tuple_idx_in_trajectory
+            + self._input_length_sequence
+        ]
+        # positions_in_sample: (sequence_length, n_particle, dimension) -> (n_particle, sequence_length, dimension)
+        positions_in_sample = np.transpose(positions_in_sample, (1, 0, 2))
+        num_particle_in_sample = positions_in_sample.shape[0]
+        label = positions[tuple_idx_in_trajectory + self._input_length_sequence]
+
+        if _material_property:
+            # We have material property in our features
+            material_property = _material_property[0]
+            assert len(material_property.shape) == 2
+            assert material_property.shape[0] == num_particle_in_sample
+
+            training_data = (
+                (
+                    torch.tensor(positions_in_sample).to(torch.float32).contiguous(),
+                    torch.tensor(particle_type).contiguous(),
+                    torch.tensor(material_property).to(torch.float32).contiguous(),
+                    num_particle_in_sample,
+                ),
+                label
+            )
+        else:
+            training_data = (
+                (
+                    torch.tensor(positions).to(torch.float32).contiguous(),
+                    torch.tensor(particle_type).contiguous(),
+                    num_particle_in_sample,
+                ),
+                label
+            )
+        
+        return training_data
+    
+    @property
+    def is_property_feature(self):
+        return len(self._trajectories_data[0]) >= 3
+
+
+def collate_fn(data):
+    """Collate function for SamplesDataset.
+
+    Args:
+        data (list): List of tuples of the form ((positions, particle_type, n_particles_per_example), label).
+
+    Returns:
+        tuple: Tuple of the form ((positions, particle_type, n_particles_per_example), label).
+    """
+    material_property_as_feature = True if len(data[0][0]) >= 4 else False
+    position_list = []
+    particle_type_list = []
+    if material_property_as_feature:
+        material_property_list = []
+    n_particles_per_example_list = []
+    label_list = []
+
+    if material_property_as_feature:
+        for (
+            positions,
+            particle_type,
+            material_property,
+            n_particles_per_example,
+        ), label in data:
+            position_list.append(positions)
+            particle_type_list.append(particle_type)
+            material_property_list.append(material_property)
+            n_particles_per_example_list.append(n_particles_per_example)
+            label_list.append(label)
+    else:
+        for (positions, particle_type, n_particles_per_example), label in data:
+            position_list.append(positions)
+            particle_type_list.append(particle_type)
+            n_particles_per_example_list.append(n_particles_per_example)
+            label_list.append(label)
+
+    if material_property_as_feature:
+        collated_data = (
+            (
+                torch.tensor(np.vstack(position_list)).to(torch.float32).contiguous(),
+                torch.tensor(np.concatenate(particle_type_list)).contiguous(),
+                torch.tensor(np.concatenate(material_property_list))
+                .to(torch.float32)
+                .contiguous(),
+                torch.tensor(n_particles_per_example_list).contiguous(),
+            ),
+            torch.tensor(np.vstack(label_list)).to(torch.float32).contiguous(),
+        )
+    else:
+        collated_data = (
+            (
+                torch.tensor(np.vstack(position_list)).to(torch.float32).contiguous(),
+                torch.tensor(np.concatenate(particle_type_list)).contiguous(),
+                torch.tensor(n_particles_per_example_list).contiguous(),
+            ),
+            torch.tensor(np.vstack(label_list)).to(torch.float32).contiguous(),
+        )
+
+    return collated_data
+
+
 # https://pytorch.org/docs/stable/data.html#torch.utils.data.Dataset
-# https://pytorch.org/tutorials/beginner/data_loading_tutorial.html?highlight=dataset
 # Dataset class for pytorch (mapping style)
 class TrajectoriesDataset(Dataset):
     """
     Trajectories dataset where each trajectory is a series of tuples
         (positions, particle_type, material_property[optional]).
-
     positions' shape: (sequence_length, n_particle, dimension)
         where dimension is the number of coordinates (e.g. 2 for x and y)
     particle type shape: (n_particles)
-
     [optional] material_property: (n_particles, feature vector)
     """
 
     def __init__(self, root_dir, transform=None):
-        self._trajectories_data = load_npz_data(root_dir)
+        self._trajectories_data = load_npz_file(root_dir)
 
     def __len__(self):
         """
@@ -87,3 +234,40 @@ class TrajectoriesDataset(Dataset):
     @property
     def is_property_feature(self):
         return len(self._trajectories_data[0]) >= 3
+
+
+def get_data_loader_by_samples(path, input_length_sequence, batch_size, shuffle=True):
+    """Returns a data loader for the dataset.
+
+    Args:
+        path (str): Path to dataset.
+        input_length_sequence (int): Length of input sequence.
+        batch_size (int): Batch size.
+        shuffle (bool, optional): Whether to shuffle the dataset. Defaults to True.
+
+    Returns:
+        torch.utils.data.DataLoader: Data loader for the dataset.
+    """
+    dataset = SamplesDataset(path, input_length_sequence)
+    return torch.utils.data.DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        pin_memory=True,
+        collate_fn=collate_fn,
+    )
+
+
+def get_data_loader_by_trajectories(path):
+    """Returns a data loader for the dataset.
+
+    Args:
+        path (str): Path to dataset.
+
+    Returns:
+        torch.utils.data.DataLoader: Data loader for the dataset.
+    """
+    dataset = TrajectoriesDataset(path)
+    return torch.utils.data.DataLoader(
+        dataset, batch_size=None, shuffle=False, pin_memory=True
+    )
--- test/test_gns.py
+++ test/test_gns.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy as np
-from gns.data_loader import load_npz_data, TrajectoriesDataset
+from gns.data_loader_AK import load_npz_file, TrajectoriesDataset, TrajectoriesSampleDataset
 
 
 class DataLoaderTest(unittest.TestCase):
@@ -13,7 +13,7 @@ class DataLoaderTest(unittest.TestCase):
         np.savez("temp.npz", **sample_data)
 
         # Test loading the data
-        loaded_data = load_npz_data("temp.npz")
+        loaded_data = load_npz_file("temp.npz")
 
         # Check if the loaded data matches the sample data
         self.assertEqual(len(loaded_data), 2)
@@ -23,7 +23,7 @@ class DataLoaderTest(unittest.TestCase):
     def test_invalid_file(self):
         # Test loading a non-existent file
         with self.assertRaises(FileNotFoundError):
-            load_npz_data("nonexistent.npz")
+            load_npz_file("nonexistent.npz")
 
     def test_trajectories_dataset_without_material_property(self):
         """
@@ -42,8 +42,8 @@ class DataLoaderTest(unittest.TestCase):
 
     def test_trajectories_dataset_with_material_property(self):
         """
-        Dataset with 30 particle, with material property, and 1,000 sequence with 3 dimension x,y,theta
-        Material property is a vector with 4 features
+        Dataset with 2 trajectories each with 30 particle, material property, and 1,000 sequence
+        with 3 dimension x,y,theta. Material property is a vector with 4 features
         """
         dataset = TrajectoriesDataset(root_dir="data/temp_material_property.npz")
         assert dataset._trajectories_data is not None
@@ -56,3 +56,35 @@ class DataLoaderTest(unittest.TestCase):
         assert data[0].shape == (30, 1000, 3)
         assert data[1].shape == (30,)
         assert data[2].shape == (30, 4)
+
+    def test_trajectories_sample_dataset(self):
+        """
+        Dataset with 2 trajectories each with 30 particle, material property, and 1,000 sequence
+        with 3 dimension x,y,theta. Material property is a vector with 4 features
+        """
+
+        dataset = TrajectoriesSampleDataset(
+            root_dir="data/temp_material_property.npz",
+            input_length_sequence=5
+        )
+        assert dataset._trajectories_data is not None
+        assert dataset.is_property_feature
+
+        # check the shapes
+        data = dataset[0]
+        assert len(data) == 2 # sequence_info, label
+        assert len(data[0]) == 4  # position, material_type, material_property, n_particle
+        assert data[0][-1] == 30
+        assert data[0][0].shape == (30, 5, 3)
+        assert data[0][1].shape == (30,)
+        assert data[0][2].shape == (30, 4)
+
+        traj_dataset = TrajectoriesDataset(
+            root_dir="data/temp_material_property.npz"
+        )
+        assert traj_dataset._trajectories_data is not None
+        assert traj_dataset.is_property_feature
+
+        # check the shapes
+        data = dataset[996] # This is equal to tuple at index 1 in second trajectory plus the next 5 points
+        np.allclose(data[1], traj_dataset[1][0][:,6,:], rtol = 1e-7)
